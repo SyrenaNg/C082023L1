af96bcb5f9f9041182e46cec36d43b419709b269
SyrenaNg <143102024+SyrenaNg@users.noreply.github.com>
2023-08-25 15:48:32 +0700

    First commit

---
+++ src/C082023L1.java
@@ -0,0 +1,21 @@
+public class C082023L1 {
+    public static void main(String[] args) {
+        System.out.println("Hello Loc!");
+
+        int myFirstVariable;
+        myFirstVariable = 1;
+        System.out.println("myFirstVariable");
+        String name = "Loc";
+        String PrintItOut = "Print this text"  + name +  myFirstVariable;
+        System.out.println(PrintItOut);
+        System.out.print(name);
+        int myNumber = 2;
+        byte myByte = 1;
+        boolean myBoolean = true;
+
+
+
+
+
+    }
+}
